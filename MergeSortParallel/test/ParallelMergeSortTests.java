eb9240d71520f39bd43a539f95fc28c0494d1a4d
MaliszewskiKamil <kamcio4112@gmail.com>
2020-11-21 16:42:52 +0100

    added extra tests

--- MergeSortParallel/test/ParallelMergeSortTests.java
+++ MergeSortParallel/test/ParallelMergeSortTests.java
@@ -37,26 +37,63 @@ public class ParallelMergeSortTests {
 
     @Test
     public void testMergeSortResultInOrder(){
-        Integer[] listToSortz = data_set;
+        Integer[] listToSort = data_set;
 
-        assertEquals((Integer)6, listToSortz[0]);
-        assertEquals((Integer)33, listToSortz[7]);
-        MergeSort.sort(listToSortz);
-        assertEquals((Integer)2, listToSortz[0]);
-        assertEquals((Integer)75, listToSortz[7]);
-        assertEquals((Integer)2, listToSortz[0]);
+        assertEquals((Integer)6, listToSort[0]);
+        assertEquals((Integer)33, listToSort[7]);
+
+        MergeSort.sort(listToSort);
+
+        assertEquals((Integer)2, listToSort[0]);
+        assertEquals((Integer)75, listToSort[7]);
+        assertEquals((Integer)2, listToSort[0]);
     }
     @Test
     public void testParallelSortResultInOrder(){
         Integer[] listToSort = data_set;
+
         assertEquals((Integer)6, listToSort[0]);
         assertEquals((Integer)33, listToSort[7]);
+
         parallelMergeSort.parallelMergeSort(listToSort, cores);
+
         assertEquals((Integer)2, listToSort[0]);
         assertEquals((Integer)75, listToSort[7]);
         assertEquals((Integer)2, listToSort[0]);
+    }
+
+    @Test
+    public void testIfMergeCanSortSizeOne(){
+        Integer[] listToSort = new Integer[1];
+        listToSort[0] = 7;
+        MergeSort.sort(listToSort);
+    }
+
+    @Test
+    public void testIfParallelCanSortSizeOne(){
+        Integer[] listToSort = new Integer[1];
+        listToSort[0] = 7;
+        parallelMergeSort.parallelMergeSort(listToSort, cores);
+    }
+
+    @Test
+    public void testIfResultsOfBothSortsMatch(){
+        Integer[] listParallel = data_set;
+        Integer[] listSequencial = data_set;
+
+        assertEquals(listParallel[0], listSequencial[0]);
+
+        parallelMergeSort.parallelMergeSort(listParallel, cores);
+        MergeSort.sort(listSequencial);
+
+        assertEquals(listParallel[0], listSequencial[0]);
+        assertEquals(listParallel[4], listSequencial[4]);
+        assertEquals(listParallel[6], listSequencial[6]);
+
+
 
     }
+
 
 
 
